7d817c2099416c43a25676cd9426038eee0788e4
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2023-12-29 21:21:11 +0000

    Initial Commit

---
+++ include/omni_drive_controller/omni_drive_controller.hpp
@@ -0,0 +1,56 @@
+/*
+    Author: Juan Pablo Gutierrez
+*/
+
+#ifndef OMNI_DRIVE_CONTROLLER__OMNI_DRIVE_CONTROLLER_HPP_
+#define OMNI_DRIVE_CONTROLLER__OMNI_DRIVE_CONTROLLER_HPP_
+
+#include "omni_drive_controller/visibility_control.h"
+#include "controller_interface/controller_interface.hpp"
+
+namespace omni_drive_controller
+{
+    class OmniDriveController : public controller_interface::ControllerInterface
+    {
+    public:
+        OmniDriveController();
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::return_type update(
+            const rclcpp::Time &time, const rclcpp::Duration &period) override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_init() override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_configure(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_activate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_deactivate(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_cleanup(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        OMNI_DRIVE_CONTROLLER_PUBLIC
+        controller_interface::CallbackReturn on_error(
+            const rclcpp_lifecycle::State &previous_state) override;
+
+        controller_interface::CallbackReturn on_shutdown(
+            const rclcpp_lifecycle::State &previous_state) override;
+    };
+}
+
+#endif
---
+++ src/omni_drive_controller.cpp
@@ -0,0 +1,5 @@
+#include "omni_drive_controller.hpp"
+
+namespace omni_drive_controller {
+
+}

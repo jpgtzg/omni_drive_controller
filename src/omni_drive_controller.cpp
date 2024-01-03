7896f635fa819ccdb036ff936473a06c2def8f09
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-03 20:39:01 +0000

    Created omni_drive_controller.cpp

--- src/omni_drive_controller.cpp
+++ src/omni_drive_controller.cpp
@@ -1,5 +1,67 @@
 #include "omni_drive_controller.hpp"
 
-namespace omni_drive_controller {
+namespace
+{
+    constexpr auto DEFAULT_COMMAND_TOPIC = "~/cmd_vel";
+    constexpr auto DEFAULT_COMMAND_UNSTAMPED_TOPIC = "~/cmd_vel_unstamped";
+    constexpr auto DEFAULT_COMMAND_OUT_TOPIC = "~/cmd_vel_out";
+    constexpr auto DEFAULT_ODOMETRY_TOPIC = "~/odom";
+    constexpr auto DEFAULT_TRANSFORM_TOPIC = "/tf";
+    constexpr auto WHEELS_QUANTITY = 4;
+} // namespace
+
+namespace omni_drive_controller
+{
+    OmniDriveController::OmniDriveController() : controller_interface::ControllerInterface() {}
+    
+    controller_interface::CallbackReturn OmniDriveController::on_init()
+    {
+        try
+        {
+            joint_names_ = {"wheel1", "wheel2", "wheel3", "wheel4"};
+            interface_names_ = {"velocity_interface"};
+
+            controller_interface::CallbackReturn result = ControllerInterface::on_init();
+            if (result != controller_interface::CallbackReturn::SUCCESS)
+            {
+                throw std::runtime_error("Failed to initialize parent ControllerInterface");
+            }
+        }
+        catch (const std::exception &e)
+        {
+            fprintf(stderr, "Exception thrown during init stage with message: %s \n", e.what());
+            return controller_interface::CallbackReturn::ERROR;
+        }
+        return controller_interface::CallbackReturn::SUCCESS;
+    }
+
+    controller_interface::CallbackReturn OmniDriveController::on_configure(const rclcpp_lifecycle::State &)
+    {
+    }
+
+    controller_interface::InterfaceConfiguration OmniDriveController::command_interface_configuration() const
+    {
+        // Implement command_interface_configuration
+    }
+
+    controller_interface::InterfaceConfiguration OmniDriveController::state_interface_configuration() const
+    {
+        // Implement state_interface_configuration
+    }
+
+    controller_interface::CallbackReturn OmniDriveController::on_activate(const rclcpp_lifecycle::State &previous_state)
+    {
+        // Implement on_activate method
+    }
+
+    controller_interface::CallbackReturn OmniDriveController::on_deactivate(const rclcpp_lifecycle::State &previous_state)
+    {
+        // Implement on_deactivate method
+    }
+
+    controller_interface::return_type OmniDriveController::update(const rclcpp::Time &time, const rclcpp::Duration &period)
+    {
+        // Implement update method
+    }
 
 }

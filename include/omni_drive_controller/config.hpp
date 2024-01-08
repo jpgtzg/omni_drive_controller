25ccef04a0c2b432916f53c7c58b4ad4c236ee01
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-08 19:19:23 +0000

    Expanded config.hpp

--- include/omni_drive_controller/config.hpp
+++ include/omni_drive_controller/config.hpp
@@ -3,9 +3,29 @@
 #ifndef OMNI_DRIVE_CONTROLLER__CONFIG_HPP_
 #define OMNI_DRIVE_CONTROLLER__CONFIG_HPP_
 
-namespace robot_config
+/* Units are in [meters], [meters/seconds], or [radians] */
+namespace robot_params
 {
+    struct RobotConfig
+    {
+        double wheel_radius;
+        double robot_radius;
+        double theta = M_PI / 2;
+    };
 
+    struct RobotVelocity
+    {
+        double vx;
+        double vy;
+        double omega;
+    };
+
+    struct RobotPosition
+    {
+        double x;
+        double y;
+        double theta;
+    };
 }
 
 #endif
